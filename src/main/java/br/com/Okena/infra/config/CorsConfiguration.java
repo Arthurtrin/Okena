1aae23f866ee795a91a86ad686f6d8fa889b86c6
Arthurtrin <arthurtrindade08102004@gmail.com>
2026-08-23 19:14:05 -0300

    feat: fix cors error

--- src/main/java/br/com/Okena/infra/config/CorsConfiguration.java
+++ src/main/java/br/com/Okena/infra/config/CorsConfiguration.java
@@ -1,6 +1,5 @@
 package br.com.Okena.infra.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,12 +8,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://192.168.1.6:5173",
-                        "http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                        "http://localhost:5173",
+                        "https://okena-hub-front.vercel.app"
+                )
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS",
+                        "HEAD",
+                        "TRACE",
+                        "CONNECT"
+                );
     }
-
 }
